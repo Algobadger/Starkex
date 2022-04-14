5dc85f15bdec4f22f9d0133ac182015607b0334b
Shadman Sakib <sakib.buet51@outlook.com>
2022-04-14 13:58:08 +0600

    StarkKey and API key derivation

---
+++ Src/Starkex.Test/TestStarkKey.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Starkex.Signatures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starkex.Test
+{
+    [TestClass]
+    public class TestStarkKey
+    {
+        private const string ExpectedTestStarkKey = "0x6c87e4377a6a8c3feeddc1afe52696daa1209720f720e853016e426d5137c60";
+        private const string ExpectedMainStarkKey = "0x553e90eb0d5eb0ef4f20a3f47fe6980df5a29e8b20bf59bc0e6d645d293af36";
+        private const string TestPvtKey = "0xd763c0550b15b05377868499ae2b0a758e0be36f945d00c427d7e17d4ee12143";
+        private static readonly StarkAPI TestApi = new("kSBivWhxB2NU5EOirsZsxjbUldGO5A4k_Azm09ff", "8a6ccef4-94d4-ad7e-8db5-1c8fb008ed65", "itJURA491_KiFJjYxQX9");
+        private static readonly StarkAPI MainApi = new("qUJoY7FvpTSYgF4vgXrTeH40o6IiHoqJ9EntqgXC", "cfe71fc1-2ff3-c635-e9a9-418123a42210", "BjAYnl5BkU6Q_41amxjv");
+
+        [TestMethod]
+        public void DeriveTestKey()
+        {
+            var starkSigner = new StarkSigner();
+            var testKey = starkSigner.DeriveStarkKey(TestPvtKey, 3);
+            Assert.AreEqual(ExpectedTestStarkKey, testKey);
+        }
+
+        [TestMethod]
+        public void DeriveMainKey()
+        {
+            var starkSigner = new StarkSigner();
+            var testKey = starkSigner.DeriveStarkKey(TestPvtKey, 1);
+            Assert.AreEqual(ExpectedMainStarkKey, testKey);
+        }
+
+        [TestMethod]
+        public void DeriveTestApI()
+        {
+            var starkSigner = new StarkSigner();
+            var testKey = starkSigner.DeriveApiKey(TestPvtKey, 3);
+            Assert.AreEqual(TestApi, testKey);
+        }
+
+        [TestMethod]
+        public void DeriveMainApI()
+        {
+            var starkSigner = new StarkSigner();
+            var mainKey = starkSigner.DeriveApiKey(TestPvtKey, 1);
+            Assert.AreEqual(MainApi, mainKey);
+        }
+    }
+}
--- Src/Starkex/Signatures/StarkSigner.cs
+++ Src/Starkex/Signatures/StarkSigner.cs
@@ -1,4 +1,7 @@
-﻿using Org.BouncyCastle.Math;
+﻿using Nethereum.Hex.HexConvertors.Extensions;
+using Nethereum.Signer;
+using Nethereum.Util;
+using Org.BouncyCastle.Math;
 using Starkex.Converters;
 using Starkex.Hashing;
 using Starkex.Models;
@@ -8,14 +11,121 @@ namespace Starkex.Signatures
 
     public class StarkSigner
     {
-        private static readonly StarkHashCalculator STARK_HASH_CALCULATOR = new StarkHashCalculator(new PedersonHash(ConstantPoints.GetECPoint(0)));
-        private static readonly EcSigner EC_SIGNER = new EcSigner(StarkCurve.GetInstance());
+        private const string TestStruct = "dYdX(string action)";
+        private const string MainStruct = "dYdX(string action,string onlySignOn)";
+        private const string DeriveStarkKeyAction = "dYdX STARK Key";
+        private const string DeriveAPIKeyAction = "dYdX Onboarding";
+        private const string MainNetDomain = "https://trade.dydx.exchange";
+        private const string DomainContract = "EIP712Domain(string name,string version,uint256 chainId)";
+        private const string Domain = "dYdX";
+        private const string Version = "1.0";
+        private const string EIP = "0x1901";
+
+        private readonly StarkHashCalculator STARK_HASH_CALCULATOR = new(new PedersonHash(ConstantPoints.GetECPoint(0)));
+        private readonly EcSigner EC_SIGNER = new(StarkCurve.GetInstance());
 
         public Signature Sign(OrderWithClientId order, NetworkId networkId, BigInteger privateKey)
         {
             var starkOrder = StarkwareOrderConverter.FromOrderWithClientId(order, networkId);
             var hash = STARK_HASH_CALCULATOR.CalculateHash(starkOrder);
             return EC_SIGNER.Sign(privateKey, hash);
+        }
+
+        public string DeriveStarkKey(string ethPrivateKey, int networkId)
+        {
+            var typedSign = GenerateSignature(ethPrivateKey, networkId, DeriveStarkKeyAction);
+            var signatureInt = Sha3Keccack.Current.CalculateHashFromHex(typedSign);
+            var big = new BigInteger(signatureInt, 16).ShiftRight(5);
+            var starkKey = big.ToString(16);
+
+            return "0x" + starkKey;
+        }
+
+        public StarkAPI DeriveApiKey(string ethPrivateKey, int networkId)
+        {
+            var typedSign = GenerateSignature(ethPrivateKey, networkId, DeriveAPIKeyAction);
+            var rHex = typedSign.Substring(0, 64);
+            var sHex = typedSign.Substring(64, 64);
+
+            var rHash = Sha3Keccack.Current.CalculateHashFromHex(rHex);
+            var sHash = Sha3Keccack.Current.CalculateHashFromHex(sHex);
+
+            var secretBytes = rHash[..60];
+            var keyBytes = sHash[..32];
+            var pass = sHash.Substring(32, 30);
+
+            _ = Guid.TryParse(keyBytes, out Guid res);
+
+            var secret = UrlSafeEncode(Convert.FromHexString(secretBytes));
+            var password =UrlSafeEncode(Convert.FromHexString(pass));
+            return new StarkAPI(secret, res.ToString(), password);
+        }
+
+        private static string GenerateSignature(string ethPrivateKey, int networkId, string action)
+        {
+            //"0x45ae2c9f941548590b88af98fafd898603592c5725b4606150e1f1bca4d3a134";
+            var structString = networkId == 1 ? MainStruct : TestStruct;
+            var allStruct = new List<string>
+            {
+                Sha3Keccack.Current.CalculateHash(structString),
+                Sha3Keccack.Current.CalculateHash(action)
+            };
+            if (networkId == 1)
+            {
+                allStruct.Add(Sha3Keccack.Current.CalculateHash(MainNetDomain));
+            }
+
+            var structhash = Sha3Keccack.Current.CalculateHashFromHex(allStruct.ToArray());
+
+
+            var contract = Sha3Keccack.Current.CalculateHash(DomainContract);
+            var domainHash = Sha3Keccack.Current.CalculateHash(Domain);
+            var versionHash = Sha3Keccack.Current.CalculateHash(Version);
+            var network = networkId.ToString().PadLeft(64, '0');
+
+            var domain = Sha3Keccack.Current.CalculateHashFromHex(contract, domainHash, versionHash, network);
+            var eipHash = Sha3Keccack.Current.CalculateHashFromHex(EIP, domain, structhash);
+
+            var eth = new EthECKey(ethPrivateKey);
+            var signed = eth.SignAndCalculateV(Convert.FromHexString(eipHash));
+
+            var rawSign = signed.R.ToHex() + signed.S.ToHex() + signed.V.ToHex();
+            var typedSign = CreateTypedSign(rawSign, 0);
+
+            return typedSign;
+           
+        }
+
+        private static string UrlSafeEncode(byte[] message)
+        {
+           return  Convert.ToBase64String(message)
+                          .TrimEnd('=')
+                          .Replace('+', '-')
+                          .Replace('/', '_');
+        }
+
+        private static string CreateTypedSign(string sign, int type)
+        {
+            if (sign.Length != 130)
+            {
+                throw new Exception("Invalid Signature");
+            }
+            var rs = sign[..128];
+            var v = sign.Substring(128, 2);
+
+            if (v == "00")
+            {
+                return rs + "1b" + $"0{type}";
+            }
+            else if (v == "01")
+            {
+                return rs + "1c" + $"0{type}";
+            }
+            else if (v == "1b" || v == "1c")
+            {
+                return sign + $"0{type}";
+            }
+            throw new Exception("Invalid Signature V");
         }
     }
 }
