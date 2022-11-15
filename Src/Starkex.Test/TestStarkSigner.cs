38cff54cc68799bff421604f56a4d89702b8a690
Shadman Sakib <sakib.buet51@gmail.com>
2022-11-15 16:50:21 +0600

    Add GOEARLI Support

--- Src/Starkex.Test/TestStarkSigner.cs
+++ Src/Starkex.Test/TestStarkSigner.cs
@@ -14,13 +14,13 @@ namespace Starkex.Test
         private static readonly DydxAsset ATOM = new("ATOM", new BigInteger("41544f4d2d37000000000000000000", 16), 7);
         private static readonly DydxAsset ZEC = new("ZEC", new BigInteger("5a45432d3800000000000000000000", 16), 8);
 
-        private static readonly string EXPECTED_SIGNATURE = "06bca455438f4e337e11dff3897d85fdd425be2eb51bd0db92f34527965934e7003376e6726048f2bb23a6f89e7a7528c8cd832493431dd6c685148517ec752f";
+        private static readonly string EXPECTED_SIGNATURE = "05b4d194ef333605d4945a4c3642879e07062d95449f00bf7749909e9b7afc92033f77986a83420cf6b0e7be90a13b9bfa8489a363e9b51727bd09fe909af60f";
         private static readonly Order order = new("12345", 145.0005, 0.125, ETH, StarkwareOrderSide.BUY, Time);
         private static readonly OrderWithClientId orderWithClientID = new OrderWithClientIdWithPrice(order,
                 "This is an ID that the client came up with to describe this order", 350.00067);
         private readonly string PRIVATE_KEY = "07230d8f6fcba9afb8eea3aa67119b5a1bc117500186c384b5aaee85dafbb64c";
         private readonly string privateKey = "0x58c7d5a90b1776bde86ebac077e053ed85b0f7164f53b080304a531947f46e3";
-        private readonly string mockSignature = "00cecbe513ecdbf782cd02b2a5efb03e58d5f63d15f2b840e9bc0029af04e8dd0090b822b16f50b2120e4ea9852b340f7936ff6069d02acca02f2ed03029ace5";
+        private readonly string mockSignature = "07670488d9d2c6ff980ca86e6d05b89414de0f2bfd462a1058fb05add68d034a036268ae33e8e21d324e975678f56b66dacb2502a7de1512a46b96fc0e106f79";
 
         [TestMethod]
         public void TestOrderWithClientIdAndQuoteAmount()
@@ -37,7 +37,7 @@ namespace Starkex.Test
             );
 
             var starkSigner = new StarkSigner();
-            Signature signature = starkSigner.Sign(order, NetworkId.ROPSTEN, PRIVATE_KEY);
+            Signature signature = starkSigner.Sign(order, NetworkId.GOERLI, PRIVATE_KEY);
             Assert.AreEqual(EXPECTED_SIGNATURE, signature.ToString());
         }
 
@@ -64,16 +64,16 @@ namespace Starkex.Test
         public void TestSingOrderOddY()
         {
             StarkSigner starkSigner = new();
-            Signature signature = starkSigner.Sign(orderWithClientID, NetworkId.ROPSTEN, privateKey);
+            Signature signature = starkSigner.Sign(orderWithClientID, NetworkId.GOERLI, privateKey);
             Assert.AreEqual(mockSignature, signature.ToString());
         }
 
         [TestMethod]
         public void TestSingOrderEvenY()
         {
-            string sign = "00fc0756522d78bef51f70e3981dc4d1e82273f59cdac6bc31c5776baabae6ec0158963bfd45d88a99fb2d6d72c9bbcf90b24c3c0ef2394ad8d05f9d3983443a";
+            string sign = "0618bcd2a8a027cf407116f88f2fa0d866154ee421cdf8a9deca0fecfda5277b03e42fa1d039522fc77c23906253e537cc5b2f392dba6f2dbb35d51cbe37273a";
             StarkSigner starkSigner = new();
-            Signature signature = starkSigner.Sign(orderWithClientID, NetworkId.ROPSTEN, "65b7bb244e019b45a521ef990fb8a002f76695d1fc6c1e31911680f2ed78b84");
+            Signature signature = starkSigner.Sign(orderWithClientID, NetworkId.GOERLI, "65b7bb244e019b45a521ef990fb8a002f76695d1fc6c1e31911680f2ed78b84");
             Assert.AreEqual(sign, signature.ToString());
         }
 
@@ -83,7 +83,7 @@ namespace Starkex.Test
             OrderWithClientId orderWithClientID = new OrderWithClientIdAndQuoteAmount(order,
                     "This is an ID that the client came up with to describe this order", 50750.272151);
             StarkSigner starkSigner = new();
-            Signature signature = starkSigner.Sign(orderWithClientID, NetworkId.ROPSTEN, privateKey);
+            Signature signature = starkSigner.Sign(orderWithClientID, NetworkId.GOERLI, privateKey);
             Assert.AreEqual(mockSignature, signature.ToString());
         }
 
--- Src/Starkex/Models/NetworkId.cs
+++ Src/Starkex/Models/NetworkId.cs
@@ -5,7 +5,7 @@ namespace Starkex.Models
     public class NetworkId
     {
         public readonly static NetworkId MAINNET = new(1, "a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", new BigInteger("02893294412a4c8f915f75892b395ebbf6859ec246ec365c3b1f56f47c3a0a5d", 16));
-        public readonly static NetworkId ROPSTEN = new(3, "8707a5bf4c2842d46b31a405ba41b858c0f876c4", new BigInteger("02c04d8b650f44092278a7cb1e1028c82025dff622db96c934b611b84cc8de5a", 16));
+        public readonly static NetworkId GOERLI = new(3, "F7a2fa2c2025fFe64427dd40Dc190d47ecC8B36e", new BigInteger("03bda2b4764039f2df44a00a9cf1d1569a83f95406a983ce4beb95791c376008", 16));
 
         public int Id { get; }
         public string AssetAddress { get; }
